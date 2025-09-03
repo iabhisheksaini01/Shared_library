00aeb3636446f1fc833399380c27d50a53cadbee
iabhisheksaini01 <sainiabhishek619@gmail.com>
2025-09-03 13:42:41 +0530

    Update go_compilation.groovy

--- vars/go_compilation.groovy
+++ vars/go_compilation.groovy
@@ -2,13 +2,13 @@ def cleanWorkspace() {
     cleanWs()
 }
 
+def checkout(String REPO_URL, String BRANCH_NAME = 'main', String credentialsId = null) {
+    git url: REPO_URL, branch: BRANCH_NAME, credentialsId: credentialsId
+}
+
 def compile(String binaryName, String mainFile) {
     sh 'go mod tidy'
     sh "go build -o ${binaryName} ${mainFile}"
-}
-
-def checkout(String REPO_URL, String BRANCH_NAME = 'main', String credentialsId = null) {
-    git url: REPO_URL, branch: BRANCH_NAME, credentialsId: credentialsId
 }
 
 
