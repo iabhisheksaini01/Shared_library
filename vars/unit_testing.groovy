07d835e4c2d18f47a64975626d3417baa82c0f35
iabhisheksaini01 <sainiabhishek619@gmail.com>
2025-09-03 13:54:16 +0530

    Update unit_testing.groovy

--- vars/unit_testing.groovy
+++ vars/unit_testing.groovy
@@ -2,6 +2,10 @@ def cleanWorkspace() {
     cleanWs()
 }
 
+def checkout(String REPO_URL, String BRANCH_NAME = 'main', String credentialsId = null) {
+    git url: REPO_URL, branch: BRANCH_NAME, credentialsId: credentialsId
+}
+
 def runTests() {
     sh 'mvn test'
 }
